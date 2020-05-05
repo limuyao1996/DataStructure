fcff2fd71197ba9cb067a7ae8659928437b92d03
Li Muyao <911407614@qq.com>
2020-05-06 01:00:08 +0800

    增加数组查找、删除操作

--- Java/src/Array.java
+++ Java/src/Array.java
@@ -1,5 +1,7 @@
-public class Array {
-    private int[] data;
+import java.util.Objects;
+
+public class Array<E> {
+    private E[] data;
     private int size;
 
     /**
@@ -7,7 +9,7 @@ public class Array {
      * @param capacity 数组容量
      */
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[])new Object[capacity];
         size = 0;
     }
 
@@ -44,7 +46,7 @@ public class Array {
      * 向数组中添加元素
      * @param e 要添加的元素
      */
-    public void addLast(int e) {
+    public void addLast(E e) {
         add(size, e);
     }
 
@@ -52,7 +54,7 @@ public class Array {
      * 在所有元素前添加元素
      * @param e 要添加的元素
      */
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
@@ -61,18 +63,108 @@ public class Array {
      * @param index 插入位置
      * @param e 要插入的值
      */
-    public void add(int index, int e) {
-        if (size == data.length) {
-            throw new IllegalArgumentException(("Add failed. Array is full."));
-        }
+    public void add(int index, E e) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed. Required index >= 0 and index <= size");
+        }
+        if (size == data.length) {
+            resize(2 * data.length);
         }
         for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
         }
         data[index] = e;
         size++;
+    }
+
+    /**
+     * 获取指定位置的元素
+     * @param index 元素下标
+     * @return 元素
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. index is illegal.");
+        }
+        return data[index];
+    }
+
+    /**
+     * 设置指定位置的元素值
+     * @param index 元素下标
+     * @param e 元素值
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Index is Illegal.");
+        }
+        data[index] = e;
+    }
+
+    /**
+     * 查找数组中是否有元素e
+     * @param e 元素值
+     */
+    public boolean contains(E e) {
+        for(int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 查找数组中元素e的索引
+     * @param e 元素值
+     */
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 删除元素
+     * @param index 要删除的元素下标
+     */
+    public E remove(int index) {
+       if (index < 0 || index >= size) {
+           throw new IllegalArgumentException("Remove failed. Index is illegal.");
+       }
+       E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        if (size == data.length / 2) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    /**
+     * 根据值删除元素
+     * @param e 想删除的元素值
+     */
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
     }
 
     @Override
@@ -90,27 +182,11 @@ public class Array {
         return res.toString();
     }
 
-    /**
-     * 获取指定位置的元素
-     * @param index 元素下标
-     * @return 元素
-     */
-    public int get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("Get failed. index is illegal.");
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
         }
-        return data[index];
-    }
-
-    /**
-     * 设置指定位置的元素值
-     * @param index 元素下标
-     * @param e 元素值
-     */
-    public void set(int index, int e) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("Set failed. Index is Illegal.");
-        }
-        data[index] = e;
+        data = newData;
     }
 }
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,7 +1,8 @@
+
 public class Main {
 
     public static void main(String[] args) {
-        Array arr = new Array(20);
+        Array<Integer> arr = new Array<Integer>();
         for (int i = 0; i < 10; i++) {
             arr.addLast(i);
         }
@@ -9,5 +10,14 @@ public class Main {
 
         arr.add(1, 100);
         System.out.println(arr.get(2));
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
     }
 }
